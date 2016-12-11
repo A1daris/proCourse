d176988462ad5619e1b77c23dfcb4a7590f12ddc
A1daris <sizov.boris@gmail.com>
2016-12-11 23:42:14 +0300

    adaptive header, tabs not work

--- js/common.js
+++ js/common.js
@@ -9,11 +9,18 @@ $(document).ready(function() {
 	});
 
 
-	//tabs
-	$(".tab_item").not(":first").hide();
+	//tabs phone
+	$(".top_phone .tab_item").not(":first").hide();
 	$(".wrapper .tab").click(function() {
 		$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	//tabs
+	$(".tabs_wrap .tab_item").not(":first").hide();
+	$(".tabs_wrap .wrapper .tab_btn").click(function() {
+		$(".tabs_wrap .wrapper .tab_btn").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_wrap .tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 
 });
